4034b1d6b7bd8e06a52ac80e93d13ac3ad78c6e0
KIWAIT1725 <ermes252020@gmail.com>
2025-08-04 15:57:13 -0600

    Se arreglo la conexion

--- app/src/config/db.php
+++ app/src/config/db.php
@@ -1,10 +1,10 @@
 <?php
 // src/config/db.php
 
-$host = "localhost";
-$dbname = "casita";
-$user = "tu_usuario";
-$password = "tu_contraseña";
+$host = "dpg-d27b7u8gjchc73fvjbqg-a";
+$dbname = "sql_postgresql_casa";
+$user = "sql_postgresql_casa_user";
+$password = "aB5mWnMnrH7BvRR67cP6sy732hVKE7nc";
 
 // Conexión a PostgreSQL usando pg_connect
 $conn = pg_connect("host=$host dbname=$dbname user=$user password=$password");
